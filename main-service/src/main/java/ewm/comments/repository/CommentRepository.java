bbfee58a69ae16ece62225f0447d4bad33c8011f
Stefan <stef.kir1999@gmail.com>
2026-04-13 23:26:50 +0400

    Рефакторинг: убрал дублирование параметров, удалил неиспользуемые методы и FORMATTER

--- main-service/src/main/java/ewm/comments/repository/CommentRepository.java
+++ main-service/src/main/java/ewm/comments/repository/CommentRepository.java
@@ -1,18 +1,10 @@
 package ewm.comments.repository;
 
 import ewm.comments.model.Comment;
-import ewm.comments.model.CommentStatus;
-import ewm.event.model.Event;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, QuerydslPredicateExecutor<Comment> {
 
-    List<Comment> findPublishedCommentsWithFilters(String text, List<Long> events, LocalDateTime rangeStart, LocalDateTime rangeEnd, Pageable pageable);
-
-    List<Comment> findByEventAndStatus(Event event, CommentStatus status);
 }
--- main-service/src/main/java/ewm/comments/service/CommentServiceImpl.java
+++ main-service/src/main/java/ewm/comments/service/CommentServiceImpl.java
@@ -24,7 +24,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.StreamSupport;
@@ -34,8 +33,6 @@ import java.util.stream.StreamSupport;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
-
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private final CommentMapper commentMapper;
     private final CommentRepository commentRepository;
